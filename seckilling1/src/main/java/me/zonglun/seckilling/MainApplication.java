1631c95e26c51c13b4e58e45815dbabc1cd02568
ZongLUnli <mrzonglunli@gmail.com>
2018-04-19 05:00:12 +0800

    project init

---
+++ seckilling1/src/main/java/me/zonglun/seckilling/MainApplication.java
@@ -0,0 +1,16 @@
+package me.zonglun.seckilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : Administrator
+ * @create 2018-04-19 4:58
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
