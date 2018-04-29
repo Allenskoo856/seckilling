3fc3b3ee200709f92d33d8bc4e997838be71783c
ZongLUnli <mrzonglunli@gmail.com>
2018-04-29 09:04:51 +0800

    change project file

--- seckilling1/src/main/java/me/zonglun/seckilling/controller/SampleController.java
+++ seckilling1/src/main/java/me/zonglun/seckilling/controller/SampleController.java
@@ -1,8 +1,14 @@
 package me.zonglun.seckilling.controller;
 
+import me.zonglun.seckilling.domain.CodeMsg;
+import me.zonglun.seckilling.domain.Result;
+import me.zonglun.seckilling.domain.User;
+import me.zonglun.seckilling.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author : Administrator
@@ -12,9 +18,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/demo/")
 public class SampleController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("hello")
     public String thyeleaf(Model model) {
         model.addAttribute("name", "allen");
         return "hello";
+    }
+
+
+    @RequestMapping("/db/get")
+    @ResponseBody
+    public Result<User> dbGet() {
+        User user = userService.getById(1);
+        return Result.success(user);
+    }
+
+    @RequestMapping("/db/tx")
+    @ResponseBody
+    public Result<Boolean> dbTx() {
+        userService.tx();
+        return Result.success(true);
     }
 }
---
+++ seckilling1/src/main/java/me/zonglun/seckilling/dao/UserDao.java
@@ -0,0 +1,26 @@
+package me.zonglun.seckilling.dao;
+
+import me.zonglun.seckilling.domain.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * @author : Administrator
+ * @create 2018-04-28 21:36
+ */
+@Mapper
+public interface UserDao {
+
+    /**
+     * 根据用户的id 查找 user
+     * @param id
+     * @return
+     */
+    @Select("SELECT * FROM user WHERE id = #{id}")
+    public User getById(@Param("id") int id) ;
+
+    @Insert("INSERT INTO user(id,name)VALUES (#{id},#{name})")
+    public int insert(User user);
+}
