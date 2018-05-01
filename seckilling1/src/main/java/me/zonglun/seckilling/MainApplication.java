ad546107bac0f001c5071b9c69e47165a01740d2
ZongLUnli <mrzonglunli@gmail.com>
2018-05-01 21:49:33 +0800

    complete

--- seckilling1/src/main/java/me/zonglun/seckilling/MainApplication.java
+++ seckilling1/src/main/java/me/zonglun/seckilling/MainApplication.java
@@ -2,15 +2,22 @@ package me.zonglun.seckilling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author : Administrator
  * @create 2018-04-19 4:58
  */
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MainApplication.class);
     }
 }
