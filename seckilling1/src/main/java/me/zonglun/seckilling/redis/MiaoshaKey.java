186f43acfe98a00b06ac1eb297feec0fba89b2db
ZongLUnli <mrzonglunli@gmail.com>
2018-05-06 09:58:58 +0800

    update changes

---
+++ seckilling1/src/main/java/me/zonglun/seckilling/redis/MiaoshaKey.java
@@ -0,0 +1,11 @@
+package me.zonglun.seckilling.redis;
+
+public class MiaoshaKey extends BasePrefix {
+
+    private MiaoshaKey(String prefix) {
+        super(prefix);
+    }
+
+    public static MiaoshaKey isGoodsOver = new MiaoshaKey("go");
+
+}
